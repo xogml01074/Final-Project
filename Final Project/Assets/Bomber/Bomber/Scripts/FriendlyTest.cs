a0e58f8b39b1e59bd9d2128a63057d6e1cd9238c
xogml01074 <xogml01074@naver.com>
2024-01-05 12:21:38 +0900

    오류 최종 수정

--- Final Project/Assets/Bomber/Bomber/Scripts/FriendlyTest.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FriendlyTest : MonoBehaviour
-{
-    public int maxHp = 20;
-    public int currentHp = 20;
-}
