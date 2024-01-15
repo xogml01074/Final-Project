b2cd3e43d63429dce8115982017b11c35c25bdce
xogml01074 <xogml01074@naver.com>
2024-01-15 19:01:27 +0900

    Revert "되돌리기"
    This reverts commit 2c0dc1fa483327ae922db41a27dc6d77d58be064.

--- Final Project/Assets/Scripts/UIManager.cs
+++ Final Project/Assets/Scripts/UIManager.cs
@@ -54,7 +54,6 @@ public class UIManager : MonoBehaviour
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
 
         StartCoroutine(Loding());
     }
@@ -68,8 +67,6 @@ public class UIManager : MonoBehaviour
 
         yield return new WaitForSeconds(3);
         loding.SetActive(false);
-=======
->>>>>>> parent of 392b659 (Attacker멀티플레이 테스트)
 =======
 >>>>>>> parent of 392b659 (Attacker멀티플레이 테스트)
 =======
