7b8ba070b034f7e0463af664a508eea22b9e0ea1
xogml01074 <xogml01074@naver.com>
2024-01-16 09:00:49 +0900

    1

--- Final Project/Assets/Scripts/UIManager.cs
+++ Final Project/Assets/Scripts/UIManager.cs
@@ -55,6 +55,7 @@ public class UIManager : MonoBehaviour
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
         StartCoroutine(Loding());
     }
@@ -68,6 +69,8 @@ public class UIManager : MonoBehaviour
 
         yield return new WaitForSeconds(3);
         loding.SetActive(false);
+=======
+>>>>>>> parent of 392b659 (Attacker멀티플레이 테스트)
 =======
 >>>>>>> parent of 392b659 (Attacker멀티플레이 테스트)
 =======
