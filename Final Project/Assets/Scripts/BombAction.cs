44a4b7400db020b5bdaac153cb86b235ba69746f
xogml01074 <xogml01074@naver.com>
2024-01-23 12:09:04 +0900

    Revert "폭탄 수정"
    This reverts commit ffcd2efa2daa070cdbe956146e911ee8fb57ac84.

--- Final Project/Assets/Scripts/BombAction.cs
+++ Final Project/Assets/Scripts/BombAction.cs
@@ -19,18 +19,23 @@ public class BombAction : NetworkBehaviour
 
     public override void Spawned()
     {
+<<<<<<< Updated upstream
         bombParticle = transform.GetChild(0).GetChild(11).GetComponent<ParticleSystem>();
+=======
         bombParticle = transform.GetChild(1).GetComponent<ParticleSystem>();
+>>>>>>> Stashed changes
     }
 
     // 충돌했을 때의 처리
     private void OnCollisionEnter(Collision collision)
     {
-        // 폭발 효과 반경 내에서 레이어가 'Enemy'(8)인 모든 게임 오브젝트들의 Collider 컴포넌트를 배열에 저장한다
-        Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
-
+<<<<<<< Updated upstream
         if (collision.gameObject.name == "Zombie1(Clone)" || collision.gameObject.name == "Zombie2(Clone)")
         {
+            Debug.Log("Zombie Hit");
+            // 폭발 효과 반경 내에서 레이어가 'Enemy'(8)인 모든 게임 오브젝트들의 Collider 컴포넌트를 배열에 저장한다
+            Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
+
             // 저장된 Collider 배열에 있는 모든 에너미에게 수류탄 데미지를 적용한다.
 
             for (int i = 0; i < cols.Length; ++i)
@@ -43,6 +48,7 @@ public class BombAction : NetworkBehaviour
         else if (collision.gameObject.name == "Titan(Clone)")
         {
             Debug.Log("Titan Hit");
+            Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
             for (int i = 0; i < cols.Length; ++i)
             {
                 cols[i].GetComponent<TitanScript>().HitEnemy(attackPower);
@@ -55,5 +61,19 @@ public class BombAction : NetworkBehaviour
             bombParticle.Play();
             Destroy(gameObject, 0.2f);
         }
+=======
+        // 폭발 효과 반경 내에서 레이어가 'Enemy'(8)인 모든 게임 오브젝트들의 Collider 컴포넌트를 배열에 저장한다
+        Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
+
+        bombParticle.Play();
+        // 저장된 Collider 배열에 있는 모든 에너미에게 수류탄 데미지를 적용한다.
+        // 여기 안됨
+        for (int i = 0; i < cols.Length; ++i)
+        {
+            cols[i].GetComponent<ZombieMovement>().Hurt(attackPower);
+            // cols[i].GetComponent<TitanScript>().HitEnemy(attackPower);
+        }
+        Destroy(gameObject, 0.1f);
+>>>>>>> Stashed changes
     }
 }
