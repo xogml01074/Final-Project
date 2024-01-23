a0006ffb527bb46a8f203114956252e77b99da41
JHKmm <149750762+JHKmm@users.noreply.github.com>
2024-01-23 11:08:31 +0900

    bomber 탄 수정

--- Final Project/Assets/Scripts/BombAction.cs
+++ Final Project/Assets/Scripts/BombAction.cs
@@ -22,24 +22,41 @@ public class BombAction : NetworkBehaviour
     public override void Spawned()
     {
         bombParticle = transform.GetChild(0).GetChild(11).GetComponent<ParticleSystem>();
-        bombParticle.Pause();
     }
-        
+
     // 충돌했을 때의 처리
     private void OnCollisionEnter(Collision collision)
     {
-        // 폭발 효과 반경 내에서 레이어가 'Enemy'(8)인 모든 게임 오브젝트들의 Collider 컴포넌트를 배열에 저장한다
-        Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
-
-        // 저장된 Collider 배열에 있는 모든 에너미에게 수류탄 데미지를 적용한다.
-        // 여기 안됨
-        for (int i = 0; i < cols.Length; ++i)
+        if (collision.gameObject.name == "Zombie1(Clone)" || collision.gameObject.name == "Zombie2(Clone)")
         {
-            cols[i].GetComponent<ZombieMovement>().Hurt(attackPower);
-            // cols[i].GetComponent<TitanScript>().HitEnemy(attackPower);
-        }
+            Debug.Log("Zombie Hit");
+            // 폭발 효과 반경 내에서 레이어가 'Enemy'(8)인 모든 게임 오브젝트들의 Collider 컴포넌트를 배열에 저장한다
+            Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
 
-        bombParticle.Play();
-        Destroy(gameObject, 0.1f);
+            // 저장된 Collider 배열에 있는 모든 에너미에게 수류탄 데미지를 적용한다.
+
+            for (int i = 0; i < cols.Length; ++i)
+            {
+                cols[i].GetComponent<ZombieMovement>().Hurt(attackPower);
+            }
+            bombParticle.Play();
+            Destroy(gameObject, 0.5f);
+        }
+        else if (collision.gameObject.name == "Titan(Clone)")
+        {
+            Debug.Log("Titan Hit");
+            Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
+            for (int i = 0; i < cols.Length; ++i)
+            {
+                cols[i].GetComponent<TitanScript>().HitEnemy(attackPower);
+            }
+            bombParticle.Play();
+            Destroy(gameObject, 0.1f);
+        }
+        else
+        {
+            bombParticle.Play();
+            Destroy(gameObject, 0.2f);
+        }
     }
 }
--- Final Project/Assets/Scripts/MineScript.cs
+++ Final Project/Assets/Scripts/MineScript.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Fusion;
+using UnityEngine;
 
 public class MineScript : NetworkBehaviour
 {
@@ -10,29 +8,37 @@ public class MineScript : NetworkBehaviour
     public ParticleSystem mineParticle;
     public GameObject mine;
     public LayerMask enemyLayer;
-    public bool isPlanted;
 
     public override void Spawned()
     {
-        mineParticle = GetComponentInChildren<ParticleSystem>();
-        mineParticle.Pause();
-        isPlanted = false;
+        mineParticle = transform.GetChild(0).GetComponent<ParticleSystem>();
     }
     private void OnTriggerEnter(Collider other)
     {
-        isPlanted = true;
-        // 폭발 효과 반경 내에서 레이어가 'Enemy'(8)인 모든 게임 오브젝트들의 Collider 컴포넌트를 배열에 저장한다
-        Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
-
-        // 저장된 Collider 배열에 있는 모든 에너미에게 수류탄 데미지를 적용한다.
-        // 여기 안됨
-        for (int i = 0; i < cols.Length; ++i)
+        if (other.gameObject.name == "Zombie1(Clone)" || other.gameObject.name == "Zombie2(Clone)")
         {
-            cols[i].GetComponent<ZombieMovement>().Hurt(mPower);
-            // cols[i].GetComponent<TitanScript>().HitEnemy(attackPower);
-        }
+            // 폭발 효과 반경 내에서 레이어가 'Enemy'(8)인 모든 게임 오브젝트들의 Collider 컴포넌트를 배열에 저장한다
+            Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
 
-        mineParticle.Play();
-        Destroy(gameObject, 0.2f);
+            // 저장된 Collider 배열에 있는 모든 에너미에게 수류탄 데미지를 적용한다.
+            // 여기 안됨
+            for (int i = 0; i < cols.Length; ++i)
+            {
+                cols[i].GetComponent<ZombieMovement>().Hurt(mPower);
+            }
+            mineParticle.Play();
+            Destroy(gameObject, 0.3f);
+        }
+        else if (other.gameObject.name == "Titan(Clone)")
+        {
+            mineParticle.Play();
+            Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
+            for (int i = 0; i < cols.Length; ++i)
+            {
+                cols[i].GetComponent<TitanScript>().HitEnemy(mPower);
+            }
+            mineParticle.Play();
+            Destroy(gameObject, 0.1f);
+        }
     }
 }
