9e6352fc5acca68be7c945b1bf35e1fa3abf5f04
xogml01074 <xogml01074@naver.com>
2024-01-23 12:20:07 +0900

    bomb 코드 간결하게 수정

--- Final Project/Assets/Scripts/BombAction.cs
+++ Final Project/Assets/Scripts/BombAction.cs
@@ -27,36 +27,23 @@ public class BombAction : NetworkBehaviour
     // 충돌했을 때의 처리
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Zombie1(Clone)" || collision.gameObject.name == "Zombie2(Clone)")
+        // 폭발 효과 반경 내에서 레이어가 'Enemy'(8)인 모든 게임 오브젝트들의 Collider 컴포넌트를 배열에 저장한다
+        Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
+
+        // 저장된 Collider 배열에 있는 모든 에너미에게 수류탄 데미지를 적용한다.
+
+        for (int i = 0; i < cols.Length; ++i)
         {
-            Debug.Log("Zombie Hit");
-            // 폭발 효과 반경 내에서 레이어가 'Enemy'(8)인 모든 게임 오브젝트들의 Collider 컴포넌트를 배열에 저장한다
-            Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
-
-            // 저장된 Collider 배열에 있는 모든 에너미에게 수류탄 데미지를 적용한다.
-
-            for (int i = 0; i < cols.Length; ++i)
+            if (cols[i].gameObject.tag == "Enemy")
             {
                 cols[i].GetComponent<ZombieMovement>().Hurt(attackPower);
             }
-            bombParticle.Play();
-            Destroy(gameObject, 0.5f);
-        }
-        else if (collision.gameObject.name == "Titan(Clone)")
-        {
-            Debug.Log("Titan Hit");
-            Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
-            for (int i = 0; i < cols.Length; ++i)
+            else if (cols[i].gameObject.tag == "Boss")
             {
                 cols[i].GetComponent<TitanScript>().HitEnemy(attackPower);
             }
-            bombParticle.Play();
-            Destroy(gameObject, 0.1f);
         }
-        else
-        {
-            bombParticle.Play();
-            Destroy(gameObject, 0.2f);
-        }
+        bombParticle.Play();
+        Destroy(gameObject, 0.5f);
     }
 }
