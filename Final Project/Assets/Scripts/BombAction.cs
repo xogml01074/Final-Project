3acd73c95cb617e2d3c802ce1aca49d1f52e8b64
JHKmm <149750762+JHKmm@users.noreply.github.com>
2024-01-25 14:40:23 +0900

    bomb 찐막

--- Final Project/Assets/Scripts/BombAction.cs
+++ Final Project/Assets/Scripts/BombAction.cs
@@ -23,15 +23,23 @@ public class BombAction : NetworkBehaviour
     // 충돌했을 때의 처리
     private void OnCollisionEnter(Collision collision)
     {
+        LayerMask eLayer = LayerMask.NameToLayer("Enemy");
+        int msk = (1 << eLayer);
         if (collision.gameObject.name == "Zombie1(Clone)" || collision.gameObject.name == "Zombie2(Clone)")
         {
             // 폭발 효과 반경 내에서 레이어가 'Enemy'(8)인 모든 게임 오브젝트들의 Collider 컴포넌트를 배열에 저장한다
-            Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
+            Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, msk);
 
-            for (int i = 0; i < cols.Length; i++)
+            foreach (Collider cs in cols)
+            {
+                if (cs.name == "Default") continue;
+                print(cs.name);
+                cs.gameObject.GetComponent<ZombieMovement>().Hurt(attackPower);
+            }
+            /*for (int i = 0; i < cols.Length; i++)
             {
                 cols[i].gameObject.GetComponent<ZombieMovement>().Hurt(attackPower);
-            }
+            }*/
             bombParticle.Play();
             Destroy(gameObject, 0.5f);
         }
--- Final Project/Assets/Scripts/MineScript.cs
+++ Final Project/Assets/Scripts/MineScript.cs
@@ -7,27 +7,31 @@ public class MineScript : NetworkBehaviour
     public float explosionRadius = 30f;
     public ParticleSystem mineParticle;
     public GameObject mine;
-    public LayerMask enemyLayer;
 
     public override void Spawned()
     {
         mineParticle = transform.GetChild(0).GetComponent<ParticleSystem>();
-        enemyLayer = LayerMask.NameToLayer("Enemy");
     }
     private void OnTriggerEnter(Collider other)
     {
+        LayerMask enemyLayer = LayerMask.NameToLayer("Enemy");
         int ELayer = (1 << enemyLayer);
         if (other.gameObject.name == "Zombie1(Clone)" || other.gameObject.name == "Zombie2(Clone)")
         {
             // 폭발 효과 반경 내에서 레이어가 'Enemy'(8)인 모든 게임 오브젝트들의 Collider 컴포넌트를 배열에 저장한다
-            Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 8);
+            Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, ELayer);
 
-            // 저장된 Collider 배열에 있는 모든 에너미에게 수류탄 데미지를 적용한다.
-            // 여기 안됨
+            foreach (Collider cs in cols)
+            {
+                if (cs.name == "Default") continue;
+                print(cs.name);
+                cs.gameObject.GetComponent<ZombieMovement>().Hurt(mPower);
+            }
+            /*
             for (int i = 0; i < cols.Length; i++)
             {
                 cols[i].gameObject.GetComponent<ZombieMovement>().Hurt(mPower);
-            }
+            }*/
             mineParticle.Play();
             Destroy(gameObject, 0.3f);
         }
